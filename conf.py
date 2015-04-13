81a544739cb3f21e08fca6604bc76c1a2fd612db
John Foster <johntfosterjr@gmail.com>
2015-04-13 00:18:40 -0500

    Remove AddThis code

--- conf.py
+++ conf.py
@@ -585,6 +585,7 @@ MathJax.Hub.Config({
 # <script src="//s7.addthis.com/js/300/addthis_widget.js#pubid=ra-4f7088a56bb93798"></script>
 # <!-- End of social buttons -->
 # """
+SOCIAL_BUTTONS_CODE = """ """
 
 # Show link to source for the posts?
 # Formerly known as HIDE_SOURCELINK (inverse)
